6f819c7923e8f35da3a107882286727e92b1cbe1
T-H-Un <39953146+T-H-Un@users.noreply.github.com>
2021-07-17 01:15:17 +0900

    Update Code
    Deleted unuse codes and change "system" to "popen" because it can down I/O load.

--- ddns.cpp
+++ ddns.cpp
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -7,22 +6,22 @@ int main(){
  FILE *file;
  char buffer[128];
  char *pcmd;
- strcpy(buffer,"curl ifconfig.io -4 > /tmp/gbip.buff");
- pcmd=(char*)buffer;
- system(pcmd);
- file=fopen("/tmp/gbip.buff","r");
- fgets(buffer,128,file);
- fclose(file);
+ file=popen("curl ifconfig.io -4","r");
+ if (file==NULL){
+	 printf("ERROR!! Can't reach \"ifconfig.io\"\n");
+	 return -1;
+	 }
+ fgets(buffer, 128, file);
+ pclose(file);
  buffer[strlen(buffer)-1]='\0';
  file=popen("openssl s_client -connect ddnsclient.onamae.com:65010 -quiet","w");
  sleep(1);
- fprintf(file,"LOGIN\nUSERID:12345678\nPASSWORD:pass1234\n.\n");
+ fprintf(file,"LOGIN\nUSERID:12345678\nPASSWORD:Pass1234\n.\n");
  sleep(1);
- fprintf(file,"MODIP\nHOSTNAME:hostname\nDOMNAME:test.com\nIPV4:%s\n.\n",buffer);
+ fprintf(file,"MODIP\nHOSTNAME:hostname\nDOMNAME:sample.com\nIPV4:%s\n.\n",buffer);
  sleep(1);
  fprintf(file,"LOGOUT\n.\n");
  pclose(file);
  printf("\n%s\n",buffer);
  return 0;
-
 }
